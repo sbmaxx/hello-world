b56450b640bc0d43e3af94a5aeb9fc577c43c62d
ishurmin <i.shurmin@yandex.ru>
2012-07-01 17:08:39 +0400

    second

--- pages/index/index.bemjson.js
+++ pages/index/index.bemjson.js
@@ -2,9 +2,9 @@
     block: 'b-page',
     title: 'index',
     head: [
-        { elem: 'css', url: '_example.css'},
-        { elem: 'css', url: '_example.ie.css', ie: 'lt IE 8' },       
-        { elem: 'js', url: 'example.js' }
+        { elem: 'css', url: '_index.css'},
+        { elem: 'css', url: '_index.ie.css', ie: 'lt IE 8' },       
+        { elem: 'js', url: 'index.js' }
     ],
     content: "Hello, world!"
 })
