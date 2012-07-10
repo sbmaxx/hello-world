3462515d57547b350a1e4095c230e8238b1e784a
Roman Rozhdestvenskiy <sbmaxx@gmail.com>
2012-07-10 09:38:38 +0400

    b-link click

--- blocks/b-link/b-link.js
+++ blocks/b-link/b-link.js
@@ -1,11 +1,14 @@
 BEM.DOM.decl('b-link', {
 
+    onSetMod: {
+        js: function() {
+            this.bindTo('click', this._onClick);
+        }
+    },
+
     _onClick: function(e) {
         console.log("click");
+        e.preventDefault();
     }
 
-}, {
-
-    
-
-})
+});
